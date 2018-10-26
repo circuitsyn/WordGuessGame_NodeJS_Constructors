ed1016c313b28d5f1701d271e7a44ffee659f259
circuitsyn <circuitsyn@gmail.com>
2018-10-26 11:59:07 -0700

    got letter.js working

--- letter.js
+++ letter.js
@@ -1,25 +1,38 @@
-function Letter(chosenChar){
-    this.currentLetter;
+var chosenChar = process.argv[2];
+var standingChar = process.argv[3];
+
+function Letter(chosenChar, standingChar) {
+    this.currentLetter = standingChar;
     this.guessedRight = false;
+    this.chosenChar = chosenChar;
     //Method to check letter and output if right or wrong
-    this.statusUpdate(chosenChar){
-        if (this.currentLetter == chosenChar){
-            console.log(chosenChar);
+    this.statusUpdate = function (){
+        if (this.currentLetter == this.chosenChar){
+            console.log(this.chosenChar);
             
         }
-        else if (this.currentLetter != chosenChar){
+        else if (this.currentLetter != this.chosenChar){
             console.log('_');
         }
-    },
+    };
 
     //Method 
-    this.guessCheck(chosenChar){
-        if (this.currentLetter == chosenChar){
+    this.guessCheck = function (){
+        if (this.currentLetter == this.chosenChar){
             this.guessedRight = true;
+            console.log(this.guessedRight);
             
         }
-        else if (this.currentLetter != chosenChar){
+        else if (this.currentLetter != this.chosenChar){
             this.guessedRight = false;
+            console.log(this.guessedRight);
         }
-    },
+    };
 };
+
+var tester = new Letter(chosenChar, standingChar);
+
+tester.statusUpdate();
+tester.guessCheck();
+
+
